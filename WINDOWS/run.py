866282a41aecaa6bab2ff355bb2bbd7b7ea93891
Susanna Cifani <73530772+susannacifani@users.noreply.github.com>
2023-11-16 19:54:31 +0100

    Create run.py

---
+++ WINDOWS/run.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.run(["python", "extract.py"])
+subprocess.run(["python", "gui.py"])
+subprocess.run(["python", "pulisci.py"])
+subprocess.run(["python", "remove_model.py"])
